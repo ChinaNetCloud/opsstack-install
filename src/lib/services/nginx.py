10865f9b3bd1dfb80c5d44bc155eacc90ef313fc
Dreamer Xiong <dreamer.xiong@MrDREAMER.local>
2016-07-12 10:16:25 +0800

    Added nginx restart variable for playbook

--- src/lib/services/nginx.py
+++ src/lib/services/nginx.py
@@ -22,7 +22,7 @@ class Nginx(abstract.Abstract):
 
     @staticmethod
     def getconf(system):
-	result = False
+		result = False
         conf_file = None 
         conf_dir = None 
         rc, out, err = utils.execute("nginx -V")
@@ -34,18 +34,21 @@ class Nginx(abstract.Abstract):
                 conf_dir = os.path.dirname(conf_file)
                 if os.path.exists(conf_file):
                     result = True
-	return result, conf_file, conf_dir
+		return result, conf_file, conf_dir
 
     @staticmethod
     def configure(system):
+		nginx_restart = "no"
         utils.out_progress_wait("Configuring '%s' monitoring..." % Nginx.getname())
         result, nginx_file, nginx_dir = Nginx.getconf(system)
-	if result == False:
-	    utils.out("Could not detect '%s' configuration path,\n" % Nginx.getname())
+		if result == False:
+	    	utils.out("Could not detect '%s' configuration path,\n" % Nginx.getname())
             utils.out("please configure manually refer to our docs: www.chinanetcloud.com/nginx-monitoring\n")
-	    return
+	    	return
+		if utils.confirm("Do you want to restart '%s' after configure monitoring?" % Nginx.getname()):
+			nginx_restart = "yes"
         if not system.config.get("nginx_monitoring_configured") == "yes":
-            rc, out, err = utils.ansible_play("rhel_nginx_monitoring", "nginx_conf_dir=%s nginx_conf_file=%s" % (nginx_file, nginx_dir))
+            rc, out, err = utils.ansible_play("rhel_nginx_monitoring", "nginx_conf_dir=%s nginx_conf_file=%s nginx_restart =%s" % (nginx_file, nginx_dir, nginx_restart))
             if rc == 0:
                 system.config.set("nginx_monitoring_configured", "yes")
                 utils.out_progress_done()
