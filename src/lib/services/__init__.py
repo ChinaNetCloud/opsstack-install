49a5ac8b3b03085d82e87f27b502ff777e0a4528
colynn.liu <colynnliu@foxmail.com>
2016-07-19 18:42:37 +0800

    php-fpm service discovery

--- src/lib/services/__init__.py
+++ src/lib/services/__init__.py
@@ -1,10 +1,14 @@
 import nginx
 import haproxy
 import apache
+import mysql
+import phpfpm
+
 
 servicelist = {
     'nginx': nginx.Nginx,
     'haproxy': haproxy.Haproxy,
     'apache': apache.Apache,
+    'phpfpm': phpfpm.Phpfpm,
     'mysql': mysql.MySQL
 }
--- src/lib/services/phpfpm.py
+++ src/lib/services/phpfpm.py
@@ -1,6 +1,5 @@
 import abstract
 import os
-import re
 
 from lib import utils
 
@@ -10,7 +9,7 @@ class Phpfpm(abstract.Abstract):
 
     @staticmethod
     def getname():
-        return 'nginx'
+        return 'phpfpm'
 
     @staticmethod
     def discover(system):
@@ -36,7 +35,7 @@ class Phpfpm(abstract.Abstract):
             utils.out("please configure manually refer to our docs: www.chinanetcloud.com/phpfpm-monitoring\n")
             return
         if utils.confirm("RESTART_PHPFPM_SERVICE?"):
-            nginx_restart = "true"
+            phpfpm_restart = "true"
         utils.out_progress_wait("CONFIGURE_PHPFPM_MONITOR")
         if not system.config.get("phpfpm_monitoring_configured") == "yes":
             rc, out, err = utils.ansible_play("rhel_phpfpm_monitoring", "phpfpm_restart=%s" % (phpfpm_restart))
