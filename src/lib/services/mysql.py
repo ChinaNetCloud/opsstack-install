a97ae774204b3c3fa978e327723d9db7b1dba979
Dreamer Xiong <dreamer.xiong@MrDREAMER.local>
2016-07-19 15:20:44 +0800

    Added newline at end of file, added condition to check if all sql is executed successfully

--- src/lib/services/mysql.py
+++ src/lib/services/mysql.py
@@ -48,21 +48,20 @@ class MySQL(abstract.Abstract):
             utils.out_progress_fail()
             utils.err("Failed to get mysql listening port")
             exit(1)
+        cmd1 = ''' -e "CREATE USER 'nccheckdb'@'localhost' IDENTIFIED BY '%s'"''' % nccheckdb_passwd
+        cmd2 = ''' -e "CREATE USER 'nccheckdb'@'127.0.0.1' IDENTIFIED BY '%s'"''' % nccheckdb_passwd
+        cmd3 = ''' -e "GRANT PROCESS, REPLICATION CLIENT on *.* TO 'nccheckdb'@'localhost'"'''
+        cmd4 = ''' -e "GRANT SELECT (Select_priv) ON mysql.user TO 'nccheckdb'@'localhost'"'''
+        cmd5 = ''' -e "GRANT PROCESS, REPLICATION CLIENT on *.* TO 'nccheckdb'@'127.0.0.1'"'''
+        cmd6 = ''' -e "GRANT SELECT (Select_priv) ON mysql.user TO 'nccheckdb'@'127.0.0.1'"'''
+        cmd7 = ''' -e "flush privileges"'''
         for port in out.strip().split('\n'):
             mysql_root = getpass.getpass("%s_MYSQL_ROOT_PASSWD:" % port)
             utils.out_progress_wait("CONFIGURE_MYSQL_%s_MONITOR" % port)
             mysql_cmd = "mysql -N -uroot -p" + mysql_root + " -P " + port
-            utils.execute(mysql_cmd +
-                          ''' -e "CREATE USER 'nccheckdb'@'localhost' IDENTIFIED BY '%s'"''' %(nccheckdb_passwd))
-            utils.execute(mysql_cmd +
-                          ''' -e "CREATE USER 'nccheckdb'@'127.0.0.1' IDENTIFIED BY '%s'"''' %(nccheckdb_passwd))
-            utils.execute(mysql_cmd +
-                          ''' -e "GRANT PROCESS, REPLICATION CLIENT on *.* TO 'nccheckdb'@'localhost'"''')
-            utils.execute(mysql_cmd +
-                          ''' -e "GRANT SELECT (Select_priv) ON mysql.user TO 'nccheckdb'@'localhost'"''')
-            utils.execute(mysql_cmd +
-                          ''' -e "GRANT PROCESS, REPLICATION CLIENT on *.* TO 'nccheckdb'@'127.0.0.1'"''')
-            utils.execute(mysql_cmd +
-                          ''' -e "GRANT SELECT (Select_priv) ON mysql.user TO 'nccheckdb'@'127.0.0.1'"''')
-            utils.execute(mysql_cmd + ''' -e "flush privileges"''')
-            utils.out_progress_done()
+            for cmd in [cmd1, cmd2, cmd3, cmd4, cmd5, cmd6, cmd7]:
+                rc, out, err = utils.execute(mysql_cmd + cmd)
+                if rc != 0:
+                    utils.out_progress_fail()
+                    exit(1)
+        utils.out_progress_done()
