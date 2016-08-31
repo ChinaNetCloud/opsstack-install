02f11da284a67b55a47deb10e2f6f016693ac3d5
Alexey Kamenskiy <alexey.kamenskiy@chinanetcloud.com>
2016-08-31 13:00:44 +0800

    Updated API class

--- src/lib/api/__init__.py
+++ src/lib/api/__init__.py
@@ -1,17 +1,9 @@
 import json
+from urllib2 import urlopen
 
 from lib import config
 from lib import log
 from lib.utils import args
-
-try:
-    # Python 2.x
-    from urllib2 import urlopen
-    from urllib2 import URLError
-except ImportError:
-    # Python 3.x
-    from urllib.request import urlopen
-    from urllib import URLError
 
 
 _singleton = None
@@ -19,10 +11,17 @@ _singleton = None
 
 # Singleton
 class _Api:
-    def __init__(self):
-        self.config = config.load()
+    def __init__(self, url):
         self.token = None
-        self.api_url = "https://opsstack-dev.service.chinanetcloud.com/api/v1%s?apikey=%s"
+        if url is not None:
+            self.api_url = url + "/api/v1%s?apikey=%s"
+        elif config.get("opsstack_api_url") is not None:
+            self.api_url = config.get("opsstack_api_url") + "/api/v1%s?apikey=%s"
+        else:
+            if args.get_args().DEV is True:
+                self.api_url = "https://opsstack-dev.service.chinanetcloud.com/api/v1%s?apikey=%s"
+            else:
+                self.api_url = "https://opsstack.chinanetcloud.com/api/v1%s?apikey=%s"
         pass
 
     def verify_token(self):
@@ -31,7 +30,7 @@ class _Api:
         result = False
         # Make sure we have self.token set
         if self.token is None:
-            token = self.config.get("api_token")
+            token = config.get("opsstack_api_token")
             if token is None:
                 return False
             else:
@@ -43,14 +42,13 @@ class _Api:
             self.token = None
         return result
 
-    def register_server(self, data):
+    def update_configuration(self, data):
         if args.get_args().DRY_RUN:
             return True
         result = False
-        success, response = self._api_call('/hosts', post_data=data)
+        method = "/hosts/%s/" % config.get('opsstack_host_id')
+        success, response = self._api_call(method, post_data=data)
         if success:
-            self.config.set('opsstack_host_id', response['data']['id'])
-            self.config.set('opsstack_host_name', response['data']['name'])
             result = True
         return result
 
@@ -58,8 +56,8 @@ class _Api:
         if args.get_args().DRY_RUN:
             return True
         result = False
-        method = "/hosts/%s/actions/monitoring.enable" % self.config.get('opsstack_host_id')
-        success, response = self._api_call(method, json.loads("{\"what\":\"that\"}"))
+        method = "/hosts/%s/actions/monitoring.enable" % config.get('opsstack_host_id')
+        success, response = self._api_call(method, {})
         if success:
             result = True
         return result
@@ -67,11 +65,11 @@ class _Api:
     def _api_call(self, method, post_data=None):
         result = (False, None)
         url = self.api_url % (method, self.token)
-        log.get_logger().log("Executing API call to %s" % url)
+        log.get_logger().debug("Executing API call to %s" % url)
         if post_data is not None:
-            log.get_logger().log("Payload is %s" % json.dumps(post_data))
+            log.get_logger().debug("Payload is %s" % json.dumps(post_data))
         else:
-            log.get_logger().log("No payload for request. Will use GET instead of POST")
+            log.get_logger().debug("No payload for request. Will use GET instead of POST")
         try:
             if post_data is not None:
                 response = urlopen(url, json.dumps(post_data))
@@ -88,21 +86,21 @@ class _Api:
                     data = response.read()
                 result = (rc, data)
                 if not rc:
-                    log.get_logger().log("Request failed with response code %d" % response.getcode())
-                    log.get_logger().log("Response content was %s" % data)
+                    log.get_logger().debug("Request failed with response code %d" % response.getcode())
+                    log.get_logger().debug("Response content was %s" % data)
             else:
                 result = (False, None)
-                log.get_logger().log("API request failed.")
-        except URLError as e:
-            log.get_logger().log("API request failed with below error")
-            log.get_logger().log(e)
+                log.get_logger().debug("API request failed.")
+        except Exception as e:
+            log.get_logger().debug("API request failed with below error")
+            log.get_logger().debug(e)
         return result
 
 
-def load():
+def load(url=None):
     global _singleton
     if _singleton is None:
-        _singleton = _Api()
+        _singleton = _Api(url)
     return _singleton
 
 
