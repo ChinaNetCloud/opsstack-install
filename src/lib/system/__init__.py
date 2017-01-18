3d6119ba3fd9b917b9b64bda1eb00a9afba2160e
dreamer.xiong <dreamer.xiong@chinanetcloud.com>
2017-01-18 15:58:12 +0800

    fix wrong playbook name issue

--- src/lib/system/__init__.py
+++ src/lib/system/__init__.py
@@ -165,7 +165,7 @@ class System:
             raise Exception("run nc-collector cron failed")
 
     def install_goaccess(self):
-        rc, out, err = utils.ansible_play("install-goaccess")
+        rc, out, err = utils.ansible_play("install_goaccess")
         if not rc == 0:
             raise Exception("goaccess installation failed")
 
