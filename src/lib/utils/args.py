e066a5b6c9553e93c43c83f0d2eaab7f37ad6084
Alexey Kamenskiy <akamensky@users.noreply.github.com>
2018-11-16 18:44:40 +0800

    Allow overwriting CLI args with envvars

--- src/lib/utils/args.py
+++ src/lib/utils/args.py
@@ -1,6 +1,8 @@
 import argparse
 import os
 
+from os import environ
+
 _args = None
 
 
@@ -40,6 +42,16 @@ def dir_validation(value):
     return result
 
 
+def _overwrite_with_envvar(curr_value, envvar_name):
+    if environ.get(envvar_name) is not None:
+        if isinstance(curr_value, bool):
+            return True
+        else:
+            return environ.get(envvar_name)
+    else:
+        return curr_value
+
+
 if _args is None:
     parser = argparse.ArgumentParser(prog="opsstack-configure", description="OpsStack installation tool. It will install required software such as monitoring etc")
     parser.add_argument('--dry-run', help=argparse.SUPPRESS, required=False, action='store_true', default=False, dest='DRY_RUN')
@@ -48,3 +60,14 @@ if _args is None:
     parser.add_argument('-y', '--assume-yes', help="Do not prompt for confirmation, always assume yes", required=False, action='store_true', default=False, dest='assume_yes')
     parser.add_argument('-b', '--batch-install', help="Add batch install tag", required=False, action='store_true', default=False, dest='batch_install')
     _args = parser.parse_args()
+    # Overwrite arguments with environment variables if provided
+    # Environment variables:
+    # OPSSTACK_CONF_FILE - path to configuration file
+    # OPSSTACK_LOG_LEVEL - logging level (debug or info only allowed)
+    # OPSSTACK_ASSUME_YES - Yes is the answer to all prompts
+    # OPSSTACK_BATCH - batch install tag
+    _args.conf_file = _overwrite_with_envvar(_args.conf_file, "OPSSTACK_CONF_FILE")
+    if environ.get("OPSSTACK_LOG_LEVEL") is not None and environ.get("OPSSTACK_LOG_LEVEL") in ['debug', 'info']:
+        _args.log_level = _overwrite_with_envvar(_args.log_level, "OPSSTACK_LOG_LEVEL")
+    _args.assume_yes = _overwrite_with_envvar(_args.assume_yes, "OPSSTACK_ASSUME_YES")
+    _args.batch_install = _overwrite_with_envvar(_args.batch_install, "OPSSTACK_BATCH")
